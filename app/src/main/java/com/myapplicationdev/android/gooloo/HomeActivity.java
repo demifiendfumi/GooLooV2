a513e836d4aadff1e051683ba55fc7fd5198a0d8
16022603 <16022603@myrp.edu.sg>
2018-06-06 14:48:43 +0800

    Alert Dialog displayble

--- app/src/main/java/com/myapplicationdev/android/gooloo/HomeActivity.java
+++ app/src/main/java/com/myapplicationdev/android/gooloo/HomeActivity.java
@@ -16,7 +16,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class HomeActivity extends AppCompatActivity {
 
     TextView tvFirstName, tvLastName;
     ImageView ivProfile;
@@ -25,8 +25,8 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     int postal_code;
     String postal;
 
-    private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mToggle;
+//    private DrawerLayout mDrawerLayout;
+//    private ActionBarDrawerToggle mToggle;
 
 
     @Override
@@ -40,16 +40,16 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         etPostalCode = (EditText) findViewById(R.id.edtPostalCode);
         btnSearch = (Button) findViewById(R.id.btnSearch);
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mToggle = new ActionBarDrawerToggle (this,mDrawerLayout, R.string.open, R.string.close);
-
-        mDrawerLayout.addDrawerListener(mToggle);
-        mToggle.syncState();
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+//        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        mToggle = new ActionBarDrawerToggle (this,mDrawerLayout, R.string.open, R.string.close);
+//
+//        mDrawerLayout.addDrawerListener(mToggle);
+//        mToggle.syncState();
+//
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//
+//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+//        navigationView.setNavigationItemSelectedListener(this);
 
         // Get intents
         Intent intent = getIntent();
@@ -84,46 +84,44 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
             }
         });
 
-
-
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(mToggle.onOptionsItemSelected(item)){
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-        int id = item.getItemId();
-
-        if (id == R.id.nav_account){
-
-            Toast.makeText(this, "account", Toast.LENGTH_SHORT).show();
-
-
-        }
-        if(id == R.id.nav_team){
-
-            Intent intent = new Intent(this,ManageTeamActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            this.startActivity(intent);
-
-        }
-        if(id == R.id.nav_order){
-
-            Toast.makeText(this, "order", Toast.LENGTH_SHORT).show();
-
-        }
-        if(id == R.id.nav_logout){
-
-            Toast.makeText(this, "logout", Toast.LENGTH_SHORT).show();
-
-        }
-        return false;
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        if(mToggle.onOptionsItemSelected(item)){
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    @Override
+//    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//
+//        int id = item.getItemId();
+//
+//        if (id == R.id.nav_account){
+//
+//            Toast.makeText(this, "account", Toast.LENGTH_SHORT).show();
+//
+//
+//        }
+//        if(id == R.id.nav_team){
+//
+//            Intent intent = new Intent(this,ManageTeamActivity.class);
+//            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//            this.startActivity(intent);
+//
+//        }
+//        if(id == R.id.nav_order){
+//
+//            Toast.makeText(this, "order", Toast.LENGTH_SHORT).show();
+//
+//        }
+//        if(id == R.id.nav_logout){
+//
+//            Toast.makeText(this, "logout", Toast.LENGTH_SHORT).show();
+//
+//        }
+//        return false;
+//    }
 }
